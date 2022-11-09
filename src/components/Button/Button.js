6344ca4cc8dc3e4fd6866a80eb28c1893af5fe2b
Martin Mazzeo <mdm271@georgetown.edu>
2022-11-09 18:08:57 -0300

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,10 @@ import * as Styled from './Button.styled'
 export function Button({ id, name, handleClick, disabled }) {
   return (
     <Styled.ButtonContainer id={id}>
-      {!disabled ? (
-        <Styled.Button onClick={handleClick}>{name}</Styled.Button>
-      ) : (
+      {disabled ? (
         <Styled.Button>{name}</Styled.Button>
+      ) : (
+        <Styled.Button onClick={handleClick}>{name}</Styled.Button>
       )}
     </Styled.ButtonContainer>
   )
